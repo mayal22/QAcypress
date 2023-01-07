cd5b1fdf48ee61cfe716146c8c40086ebbe0f84f
mayal22 <amsharma750@gmail.com>
2023-01-07 13:01:04 +0530

    Initial commit

---
+++ cypress/e2e/first_test.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+describe("assertions",()=>{
+
+  it("not assertion",()=>{
+    expect("atul").to.not.be.equal("ATUL")
+  })
+  it("not equal again",()=>{
+    expect(true).not.to.be.equal(false)
+  })
+
+  it.only("is not equal",()=>{
+    expect(21).is.not.equal(14)
+  })
+})
+
+describe("deep expect assertion",()=>{
+  it.only("deep not equal",()=>{
+    let one_object = {
+      "name":"Rahul bhuj",
+      "age": 41
+    }
+    let other_object = {
+      "age":41,
+      "name":"Rahul bhujjj"
+      
+    }
+    expect(one_object).to.be.deep.equal(other_object)
+  })
+})
---
+++ cypress/e2e/jquery.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress"/>
+
+describe("find elements by jquery",()=>{
+    it("find element by css",()=>{
+        cy.visit("https://www.orangehrm.com/")
+        console.log(cy.url())
+        
+    })
+})
